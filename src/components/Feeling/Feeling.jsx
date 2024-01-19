400c46b66f86a51cd7c10c03b0d6d0458f3cb295
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-01-19 09:11:47 -0600

    Feeling boilerplate setup. Heading, input, button. Button proceeds to linked route. Need to setup local state, store, reducers, pass input to state, then call function to send data to store

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const Feeling = () => {
     console.log("in Feeling()");
 
     return(
         <>
-            <h1>Feeling</h1>
+        <div className="compContainer">
+            <h1>How are you feeling today?</h1>
+            <h3>Feeling?</h3>
+            <div className="rating">
+                <input type="number" placeholder="Feeling on scale: 1-10?"/>
+                <Link to='/understanding'><button>Next</button></Link>
+            </div>
+        </div>
         </>
     )
 }
